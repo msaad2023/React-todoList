7e5ddb859f480baefc66c021d1f3d2e56684b674
msaad2023 <alschichan@gmail.com>
2023-06-24 14:38:04 -0700

    Add all my files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoList from "./Components/TodoList";
+const App = () => {
+  return (
+    <React.Fragment>
+      <TodoList/>
+    </React.Fragment>
+  );
+};
+
+export default App;
